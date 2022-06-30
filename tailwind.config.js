2cdba1318e2d6d7193a48344a3a12d551bf61514
mkfizi <mkfizi93@gmail.com>
2022-06-30 15:26:16 +0800

    Rewrite `app.js`, add workaround fix to address viewport height issue on mobile browsers

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,21 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
     mode: 'jit',
     darkMode: 'class',
     content: [
-      "./*.html",
-      "./src/**/*.{html,js}"
+        "./*.html",
+        "./src/**/*.{html,js}"
     ],
-    theme: {
-      extend: {},
+    theme: {	
+        extend: {
+            minHeight: {
+                "screen": [defaultTheme.minHeight.screen, "calc(var(--vh, 1vh) * 100)"],
+            },
+            height: {
+                "screen": [defaultTheme.height.screen, "calc(var(--vh, 1vh) * 100)"],
+            }
+        },
     },
     plugins: [],
 }
